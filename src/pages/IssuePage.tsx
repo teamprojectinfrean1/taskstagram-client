cc473aa14938041d8136056606302cc9cfc4cc5b
Soo Park <sooparksb@gmail.com>
2024-02-05 12:05:32 +0900

    이슈 페이지 기본 레이아웃

--- src/pages/IssuePage.tsx
+++ src/pages/IssuePage.tsx
@@ -1,9 +1,32 @@
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
+import IssueTicketContainer from "@/components/IssueTicketContainer";
+import IssueStoryContainer from "@/components/IssueStoryContainer";
+import { Box } from "@mui/material";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import DeleteIcon from "@mui/icons-material/Delete";
+
 const IssuePage = () => {
-    return (
-      <div >
-       이슈 페이지
-      </div>
-    );
-  };
-  
-  export default IssuePage;
+  return (
+    <Box display="flex" flexDirection="column" gap={4} sx={{height: '100%'}}>
+      <IssueStoryContainer
+        title="진행 중"
+        IconComponent={KeyboardDoubleArrowRightIcon}
+        ariaLabel="next"
+      />
+      <Box display="flex" gap={4} width="100%">
+        <IssueTicketContainer
+          title="할 일"
+          IconComponent={AddCircleIcon}
+          ariaLabel="create issue"
+        />
+        <IssueTicketContainer
+          title="완료"
+          IconComponent={DeleteIcon}
+          ariaLabel="delete issue"
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default IssuePage;
