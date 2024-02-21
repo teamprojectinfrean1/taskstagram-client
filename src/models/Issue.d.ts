c7152dbd0e5c770b953715cd5ad884bf14fd18ba
Soo Park <sooparksb@gmail.com>
2024-02-22 08:29:21 +0900

    주석 제거

--- src/models/Issue.d.ts
+++ src/models/Issue.d.ts
@@ -3,7 +3,7 @@ import { RawDraftContentState } from "draft-js";
 export type IssueFormData = {
   title: string | null;
   content: RawDraftContentState | null;
-  assignee: string[] | null; // assignee는 객체여야 함; image url, name, 등등의 정보를 담은 객체
+  assignee: string[] | null; 
   task: string | null;
   dateRange: [string, string] | null;
   type: string | null;
@@ -14,4 +14,4 @@ export type Issue = {
   
 };
 
-/* Issue 랑 Issue Form Data 랑 분리할 필요가 있을까? */
+
