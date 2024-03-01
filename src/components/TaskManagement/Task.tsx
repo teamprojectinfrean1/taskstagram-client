eb42464f3d9c1f94e91f9a51c38169df5f934714
hyori100 <ag497752@naver.com>
2024-03-02 01:44:56 +0900

    Task관리 페이지 레이아웃 수정(TaskCard header&content에 text-overflow 및 디자인 디테일 반영)

--- src/components/TaskManagement/Task.tsx
+++ src/components/TaskManagement/Task.tsx
@@ -1,63 +1,101 @@
-import { Card, CardContent, CardHeader, IconButton, Typography } from '@mui/material';
-import theme from '@/theme/theme';
-import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
-import TaskObj from '@/models/TaskObj';
-import { RawDraftContentState, convertFromRaw } from 'draft-js';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import theme from "@/theme/theme";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import TaskObj from "@/models/TaskObj";
+import { RawDraftContentState, convertFromRaw } from "draft-js";
 
 type TaskProps = {
-    selectedTask:TaskObj;
-    onDelete(task:TaskObj): void;
-    onShowTaskModal(isModalOpen:boolean): void;
-    onSelectedTask(selectedTask:TaskObj): void;
+  selectedTask: TaskObj;
+  onDelete(task: TaskObj): void;
+  onShowTaskModal(isModalOpen: boolean): void;
+  onSelectedTask(selectedTask: TaskObj): void;
 };
 
-const Task = ({selectedTask, onDelete, onShowTaskModal, onSelectedTask}:TaskProps) => {
-    const onClick = () => {
-        onShowTaskModal(true);
-        onSelectedTask(selectedTask);
-    }
-    return (
-        <Card sx={{height:"200px", 
-            borderRadius: 4, 
-            p:1, 
-            boxShadow:3, 
-            background:theme.palette.primary.light,
-            "&:hover": {
-                backgroundColor: theme.palette.secondary.light
-            }}} 
-            variant="elevation" 
-            square={false}> 
-            <CardHeader
-                sx={{'&:hover': {cursor: 'pointer'},
-                "& .MuiCardHeader-content": {
-                  overflow: "hidden"
-                }}}
-                onClick={onClick}
-                action={
-                    <IconButton onClick={(e) => {
-                        e.stopPropagation();
-                        onDelete(selectedTask);
-                    }}>
-                        <DeleteOutlineIcon />
-                    </IconButton>
-                } 
-                title={selectedTask.taskName}
-                titleTypographyProps={{ noWrap: true, variant:"h6", fontWeight:"bold" }}>
-            </CardHeader>
-            <CardContent sx={{height:"calc(100% - 70px)", backgroundColor: "white", borderRadius: 3, mx:1, '&:hover': {cursor: 'pointer'}}} 
-                onClick={onClick}>
-                {selectedTask.taskExplanation && 
-                    <Typography variant="subtitle1"
-                        sx={{display:"-webkit-box", 
-                            WebkitLineClamp:3, 
-                            WebkitBoxOrient:'vertical', 
-                            overflow:"hidden", 
-                            textOverflow:"ellipsis"}}>
-                        {convertFromRaw(selectedTask.taskExplanation as RawDraftContentState).getPlainText()}
-                    </Typography>}
-            </CardContent>
-        </Card>
-    )
-}
+const Task = ({
+  selectedTask,
+  onDelete,
+  onShowTaskModal,
+  onSelectedTask,
+}: TaskProps) => {
+  const onClick = () => {
+    onShowTaskModal(true);
+    onSelectedTask(selectedTask);
+  };
+  return (
+    <Card
+      sx={{
+        height: "200px",
+        borderRadius: 4,
+        p: 1,
+        boxShadow: 3,
+        background: theme.palette.primary.light,
+        "&:hover": {
+          backgroundColor: theme.palette.secondary.light,
+        },
+      }}
+      variant="elevation"
+      square={false}
+    >
+      <CardHeader
+        sx={{
+          "&:hover": { cursor: "pointer" },
+          "& .MuiCardHeader-content": {
+            overflow: "hidden",
+          },
+        }}
+        onClick={onClick}
+        action={
+          <IconButton
+            onClick={(e) => {
+              e.stopPropagation();
+              onDelete(selectedTask);
+            }}
+          >
+            <DeleteOutlineIcon />
+          </IconButton>
+        }
+        title={selectedTask.taskName}
+        titleTypographyProps={{
+          noWrap: true,
+          variant: "h6",
+          fontWeight: "bold",
+        }}
+      ></CardHeader>
+      <CardContent
+        sx={{
+          height: "calc(100% - 70px)",
+          backgroundColor: "white",
+          borderRadius: 3,
+          mx: 1,
+          "&:hover": { cursor: "pointer" },
+        }}
+        onClick={onClick}
+      >
+        {selectedTask.taskExplanation && (
+          <Typography
+            variant="subtitle1"
+            sx={{
+              display: "-webkit-box",
+              WebkitLineClamp: 3,
+              WebkitBoxOrient: "vertical",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+            }}
+          >
+            {convertFromRaw(
+              selectedTask.taskExplanation as RawDraftContentState
+            ).getPlainText()}
+          </Typography>
+        )}
+      </CardContent>
+    </Card>
+  );
+};
 
 export default Task;
--- src/pages/TaskPage.tsx
+++ src/pages/TaskPage.tsx
@@ -2,46 +2,49 @@ import Task from "@/components/TaskManagement/Task";
 import NewTask from "@/components/TaskManagement/NewTask";
 import TaskModal from "@/components/TaskManagement/TaskModal";
 import { useState, useEffect } from "react";
-import { Grid, Box, Typography, Pagination } from '@mui/material';
+import { Grid, Box, Typography, Pagination } from "@mui/material";
 import TaskObj from "@/models/TaskObj";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { taskListState } from "@/stores/Store";
 
-
 const TaskPage = () => {
   const [taskList, setTaskList] = useRecoilState(taskListState);
   const [showModal, setShowModal] = useState(false);
-  const [selectedTask, setSelectedTask] = useState<TaskObj|null>();
+  const [selectedTask, setSelectedTask] = useState<TaskObj | null>();
   const [currentPage, setCurrentPage] = useState(1);
 
-  useEffect(()=>{
-    setCurrentPage(Math.ceil((taskList.length+1)/8));
-  },[taskList.length]);
+  useEffect(() => {
+    setCurrentPage(Math.ceil((taskList.length + 1) / 8));
+  }, [taskList.length]);
 
   //util에 주입예정
-  const replaceItemAtIndex = (arr:TaskObj[], index:number, newValue:TaskObj) => {
+  const replaceItemAtIndex = (
+    arr: TaskObj[],
+    index: number,
+    newValue: TaskObj
+  ) => {
     return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
-  }
+  };
 
   //util에 주입예정
-  const removeItemAtIndex = (arr:TaskObj[], index:number) => {
+  const removeItemAtIndex = (arr: TaskObj[], index: number) => {
     return [...arr.slice(0, index), ...arr.slice(index + 1)];
-  }
+  };
 
-  const addTask = (task:TaskObj) => {
+  const addTask = (task: TaskObj) => {
     setTaskList((oldTaskList) => [...oldTaskList, task]);
     //해당 task create하는 api 호출로 대체 예정
     //setCurrentPage();
   };
 
-  const replaceTask = (previousTask:TaskObj, newTask:TaskObj) => {
+  const replaceTask = (previousTask: TaskObj, newTask: TaskObj) => {
     const index = taskList.findIndex((listItem) => listItem === previousTask);
     const newList = replaceItemAtIndex(taskList, index, newTask);
     setTaskList(newList);
     //해당 task update하는 api 호출로 대체 예정
-  }
+  };
 
-  const deleteTask = (task:TaskObj) => {
+  const deleteTask = (task: TaskObj) => {
     const index = taskList.findIndex((listItem) => listItem === task);
     const newList = removeItemAtIndex(taskList, index);
 
@@ -53,14 +56,17 @@ const TaskPage = () => {
   const hanldeCloseTaskModal = () => {
     setShowModal(false);
     //전체 task select하는 api 호출 후 setTaskList();
-  }
+  };
 
-  const handlePaginationChange = (e:React.ChangeEvent<unknown>, value:number) => {
+  const handlePaginationChange = (
+    e: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
     setCurrentPage(value);
-  }
+  };
 
   // useEffect(()=>{
-  //   const taskobjs:TaskObj[] = Array.from(Array(7)).map((_, index) => 
+  //   const taskobjs:TaskObj[] = Array.from(Array(7)).map((_, index) =>
   //     {
   //       return {
   //       taskId: `TaskId${index+1}`,
@@ -70,45 +76,64 @@ const TaskPage = () => {
   //   setTaskList([...taskobjs]);
   // }, []);
 
-    return (
-      <div>
-        <Grid container direction="column" spacing={1} p={5}>
-          <Typography
-            variant="h5"
-            sx={{ borderBottom: "1px solid black", width:"65px", fontWeight:"bold" }}>
-            TASK
-          </Typography>
-          <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }} sx={{height: "100%", minHeight: "450px", m:1}}>
-            {taskList.length > 0 ? taskList.map(task =>
-              <Grid item xs={12} md={3} key={task.taskId}>
-                <Task key={task.taskId}
-                  selectedTask={task}
-                  onDelete={deleteTask}
-                  onShowTaskModal={setShowModal}
-                  onSelectedTask={setSelectedTask}
-                />
-              </Grid>) : null}
-              <Grid item xs={12} md={3}>
-                <NewTask onClick={setSelectedTask} onShowTaskModal={setShowModal}></NewTask>
-              </Grid>
+  return (
+    <div>
+      <Grid container direction="column" spacing={1} p={5}>
+        <Typography
+          variant="h5"
+          sx={{
+            borderBottom: "1px solid black",
+            width: "65px",
+            fontWeight: "bold",
+          }}
+        >
+          TASK
+        </Typography>
+        <Grid
+          container
+          rowSpacing={2}
+          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          sx={{ height: "100%", minHeight: "450px", m: 1 }}
+        >
+          {taskList.length > 0
+            ? taskList.map((task) => (
+                <Grid item xs={12} md={3} key={task.taskId}>
+                  <Task
+                    key={task.taskId}
+                    selectedTask={task}
+                    onDelete={deleteTask}
+                    onShowTaskModal={setShowModal}
+                    onSelectedTask={setSelectedTask}
+                  />
+                </Grid>
+              ))
+            : null}
+          <Grid item xs={12} md={3}>
+            <NewTask
+              onClick={setSelectedTask}
+              onShowTaskModal={setShowModal}
+            ></NewTask>
           </Grid>
-          <Box sx={{display:"flex", justifyContent:"center", m:3}}>
-            <Pagination 
-              count={Math.ceil((taskList.length+1)/8)}
-              page={currentPage}
-              onChange={handlePaginationChange}
-              shape="rounded"/>
-          </Box>
         </Grid>
-        <TaskModal selectedTask={selectedTask as TaskObj} 
-          isOpen={showModal} 
-          onAdd={addTask}
-          onReplace={replaceTask}
-          onDelete={deleteTask}
-          onCloseModal={hanldeCloseTaskModal}></TaskModal>
-        
-      </div>
-    );
-  };
-  
-  export default TaskPage;
+        <Box sx={{ display: "flex", justifyContent: "center", m: 3 }}>
+          <Pagination
+            count={Math.ceil((taskList.length + 1) / 8)}
+            page={currentPage}
+            onChange={handlePaginationChange}
+            shape="rounded"
+          />
+        </Box>
+      </Grid>
+      <TaskModal
+        selectedTask={selectedTask as TaskObj}
+        isOpen={showModal}
+        onAdd={addTask}
+        onReplace={replaceTask}
+        onDelete={deleteTask}
+        onCloseModal={hanldeCloseTaskModal}
+      ></TaskModal>
+    </div>
+  );
+};
+
+export default TaskPage;
