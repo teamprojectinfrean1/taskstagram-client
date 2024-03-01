2b610f595982762685bb33407db91082b8d8ecf3
hyori100 <ag497752@naver.com>
2024-03-02 01:44:56 +0900

    Task관리 페이지 레이아웃 수정(TaskCard header&content에 text-overflow 및 디자인 디테일 반영)

--- src/components/TaskManagement/NewTask.tsx
+++ src/components/TaskManagement/NewTask.tsx
@@ -1,41 +1,47 @@
-import { Card, IconButton, Typography } from '@mui/material';
-import theme from '@/theme/theme';
-import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
-import TaskObj from '@/models/TaskObj';
+import { Card, IconButton, Typography } from "@mui/material";
+import theme from "@/theme/theme";
+import AddCircleRoundedIcon from "@mui/icons-material/AddCircleRounded";
+import TaskObj from "@/models/TaskObj";
 
 type NewTaskProps = {
-    onShowTaskModal(isModalOpen:boolean): void;
-    onClick(task:TaskObj|null): void;
-}
+  onShowTaskModal(isModalOpen: boolean): void;
+  onClick(task: TaskObj | null): void;
+};
 
-const NewTask = ({onClick, onShowTaskModal}:NewTaskProps) => {
-    return (
-        <Card 
-            onClick={() => {
-                onClick(null);
-                onShowTaskModal(true);
-            }}
-            sx={{
-                height:"200px",
-                borderRadius: 4,
-                boxShadow:3,
-                p:1, 
-                background:theme.palette.primary.light,
-                display:"flex",
-                flexDirection:"column",
-                justifyContent:"center",
-                alignItems:"center",
-                "&:hover": {
-                    backgroundColor: theme.palette.secondary.light,
-                    cursor: 'pointer'
-                }}} 
-            variant="elevation" 
-            square={false}>
-            <AddCircleRoundedIcon fontSize="large"/>
-            <Typography variant="subtitle1" sx={{textAlign:"center", fontWeight:"bold"}}>TASK</Typography>
-        </Card>
-    )
-
-}
+const NewTask = ({ onClick, onShowTaskModal }: NewTaskProps) => {
+  return (
+    <Card
+      onClick={() => {
+        onClick(null);
+        onShowTaskModal(true);
+      }}
+      sx={{
+        height: "200px",
+        borderRadius: 4,
+        boxShadow: 3,
+        p: 1,
+        background: theme.palette.primary.light,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        "&:hover": {
+          backgroundColor: theme.palette.secondary.light,
+          cursor: "pointer",
+        },
+      }}
+      variant="elevation"
+      square={false}
+    >
+      <AddCircleRoundedIcon fontSize="large" />
+      <Typography
+        variant="subtitle1"
+        sx={{ textAlign: "center", fontWeight: "bold" }}
+      >
+        TASK
+      </Typography>
+    </Card>
+  );
+};
 
 export default NewTask;
