77af437f96685824c36eafb2975609c8d9d536ba
sfdjsh <sfdjsh@naver.com>
2024-04-28 21:59:09 +0900

    BASE_URL 테스트

--- src/apis/domainSettings.ts
+++ src/apis/domainSettings.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://d2wzo46dl5rumb.cloudfront.net/api/v1/";
+const BASE_URL = "https://d2wzo46dl5rumb.cloudfront.net/";
 
 const unauthorizedAxios = axios.create({
   baseURL: BASE_URL,
