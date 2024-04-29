2c37316824cebd9ed7c602f668803e65886ad861
sooparkdev <135008690+sooparkdev@users.noreply.github.com>
2024-04-30 00:38:46 +0900

    getPaginatedProjectMemberList 함수로 대체 가능하여 getUserStoryList 함수 삭제

--- src/apis/userApi.ts
+++ src/apis/userApi.ts
@@ -1,4 +1,4 @@
-import { authorizedAxios, unauthorizedAxios } from "./domainSettings";
+import { unauthorizedAxios } from "./domainSettings";
 
 const userPath = "users";
 
@@ -16,37 +16,5 @@ export const getUserInfo = async () => {
     return userInfo;
   } catch (err) {
     console.error(err);
-  }
-};
-
-type UserStoryListRequest = {
-  projectId: string;
-  page: number;
-};
-
-type UserStoryListResponse = {
-  dataList: IssueStory[];
-  hasMore: boolean;
-};
-
-export const getUserStoryList = async ({
-  projectId,
-  page,
-}: UserStoryListRequest): Promise<UserStoryListResponse> => {
-  const USER_PER_PAGE = 15;
-
-  try {
-    const response = await authorizedAxios.post(
-      `${userPath}/project/user-list`,
-      {
-        projectId,
-        USER_PER_PAGE,
-        page,
-      }
-    );
-    const data = response.data;
-    return { dataList: data.data, hasMore: data.hasMore };
-  } catch (error) {
-    throw new Error("이슈 스토리 목록을 가져오는 중 오류가 발생했습니다");
   }
 };
