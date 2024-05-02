c54f1b46781258e24ddc686b01a30fdaa570b898
sfdjsh <sfdjsh@naver.com>
2024-05-03 00:14:58 +0900

    base_url 수정

--- src/apis/domainSettings.ts
+++ src/apis/domainSettings.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://124.61.74.148:8080/api/v1/";
+const BASE_URL = "https://mcpark.info/api/v1/";
 
 const unauthorizedAxios = axios.create({
   baseURL: BASE_URL,
