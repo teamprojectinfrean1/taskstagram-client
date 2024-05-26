3cbcc2d5d238bf5f1177a59ec4d204e5bf34d0a5
sfdjsh <sfdjsh@naver.com>
2024-05-26 21:34:34 +0900

    회원 탈퇴 api 수정

--- src/apis/member/deleteUser.ts
+++ src/apis/member/deleteUser.ts
@@ -2,7 +2,6 @@ import { authorizedAxios } from "../domainSettings";
 import { memberPath } from "./memberSettings";
 
 export const deleteUser = async (memberId: string) => {
-  console.log(memberId);
   try {
     const response = await authorizedAxios.delete(
       `${memberPath}?memberId=${memberId}`
